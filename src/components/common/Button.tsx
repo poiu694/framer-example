7256a21a573709a5869db6ccdb993ada18c21f96
Lee <43488305+poiu694@users.noreply.github.com>
2022-09-06 11:22:21 +0900

    feat: Common Component 생성

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,29 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ComponentProps, CSSProperties, PropsWithChildren } from 'react';
+
+interface Props extends ComponentProps<'button'> {
+  width?: CSSProperties['width'];
+  disabled?: boolean;
+}
+
+function Button({ width, children, ...restProps }: PropsWithChildren<Props>) {
+  return (
+    <Wrapper width={width} {...restProps}>
+      {children}
+    </Wrapper>
+  );
+}
+
+type StyleProps = Pick<Props, 'width' | 'disabled'>;
+
+const Wrapper = styled.button<StyleProps>`
+  ${({ disabled, width }) => css`
+    width: ${typeof width === 'number' ? width : `${width}px`};
+    border: none;
+    outline: none;
+    cursor: ${disabled ? 'default' : 'pointer'};
+  `}
+`;
+
+export default Button;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,1 +1,4 @@
-export { default as SideMenu } from './SideMenu';
+export { default as Button } from './Button';
+export { default as SideMenuItem } from './SideMenuItem';
+export { default as SideMenuList } from './SideMenuList';
+export { default as Typography } from './Typography';
