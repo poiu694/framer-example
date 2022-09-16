4704103465994d3f01c5f45dcc1ee35d9d5956f9
Lee <43488305+poiu694@users.noreply.github.com>
2022-09-16 15:36:03 +0900

    style: Carousel component into directory

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './common';
+export * from './Carousel';
 export { default as TestFramerComponent } from './TestFramerComponent';
 export { default as SideMenuToggleButton } from './SideMenuToggleButton';
 export { default as SideMenuWithToggleButton } from './SideMenuWithToggleButton';
 export { default as RefreshButton } from './RefreshButton';
-export { default as CarouselCard } from './CarouselCard';
-export { default as CarouselCardList } from './CarouselCardList';
